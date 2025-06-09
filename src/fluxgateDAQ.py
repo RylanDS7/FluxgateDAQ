12e8da71aec271097bf8129471b2ceccec6a0fc5
RylanDS7 <ryds105@gmail.com>
2025-06-09 14:46:47 -0700

    Working read single

---
+++ src/fluxgateDAQ.py
@@ -0,0 +1,143 @@
+"""
+Read analog output from Bartington fluxgate using a Labjack device
+Rylan Stutters
+June 2025
+
+Adapted from code by Derek Fujimoto:
+https://github.com/ucn-triumf/QZFM/blob/main/src/QZFM/LabJack.py
+
+Requires install of the following:
+    Labjack
+        LJM software for T7 devices: https://labjack.com/pages/support?doc=%2Fsoftware-driver%2Finstaller-downloads%2Fljm-software-installers-t4-t7-digit%2F
+        LJM python package: python -m pip install labjack-ljm
+"""
+
+import numpy as np
+import pandas as pd
+import time
+from datetime import datetime
+
+# import labjack-ljm
+try:
+    from labjack import ljm
+except ModuleNotFoundError:
+    pass
+
+class fluxgateLJ:
+    """Low-level readback from Labjack DAQ module via USB
+
+    Attributes:
+
+        ch: (tuple): channel addresses for analog inputs
+        handle (int): handle for sending info to labjack. Output of ljm.openS
+    """
+
+    def __init__(self, fluxgate_name=None, LJ_type='T7', LJ_connection='USB', LJ_id='ANY',
+                 fluxgate_nbytes=1000):
+        """Initialize object: connect
+
+        Args:
+            fluxgate_name (str): name of device to look for (connection)
+                             for windows this is likely COM3 or COM5
+                             for linux, search Z3T0 or similar
+            LJ_type (str): passed to ljm.openS, model type
+            LJ_connection (str): passed to ljm.openS, connection type. USB|ANY|ETHERNET
+            LJ_identifier (str): passed to ljm.openS, device id
+            fluxgate_nbytes (int): serial read chunk size in bytes for status updates
+        """
+
+        # get LJ handle
+        self.handle = ljm.openS(deviceType=LJ_type,
+                                connectionType=LJ_connection,
+                                identifier=LJ_id)
+        
+
+    def setup(self, x=0, y=1, z=2):
+        """Setup input channels to read from. Use AI#
+
+        Args:
+            x (int): AI# channel to read in Bx
+            y (int): AI# channel to read in By
+            z (int): AI# channel to read in Bz
+        """
+
+        # check inputs
+        if not (type(x) is int and type(y) is int and type(z) is int):
+            raise RuntimeError(f'x, y, and z must be int, not {type(x)}, {type(y)}, and {type(z)}')
+
+        if x == y or y == z or x == z:
+            raise RuntimeError('Require that x != y != z')
+
+        # get channel addresses
+        self.ch = ljm.namesToAddresses(3, (f'AIN{x}', f'AIN{y}', f'AIN{z}'))[0]
+
+    def read_single(self):
+        """Read single set of values from device. Use read_data get a longer sequence
+
+        Returns:
+            np.ndarray: fields in uT
+        """
+        dataTypes = [ljm.constants.FLOAT32, ljm.constants.FLOAT32,
+              ljm.constants.FLOAT32]
+        nchannels = len(self.ch)
+
+        # read data
+        dat = np.array(ljm.eReadAddresses(self.handle, nchannels, self.ch, dataTypes))
+
+        return dat
+
+    def to_csv(self, filename=None, *notes):
+        """Write data to csv, if no filename, use default
+
+        Args:
+            filename (str): name of file to write
+            notes: things to add to file header
+        """
+
+        # set default file name
+        if filename is None:
+            t = datetime.now()
+            filename = f'fluxgate_{datetime.strftime("%y%m%d%H%M%S")}.csv'
+
+        # make dataframe
+        df = self.field
+        self.update_status()
+
+        # write file header
+        header = [ '# fluxgate analog data',
+                   '# ',
+                  f'# Field zeroed:    {self.led["field zeroed (LED4)"]}',
+                  f'# Laser lock:      {self.led["laser lock (LED3)"]}',
+                  f'# Cell T lock:     {self.led["cell temp lock (LED2)"]}',
+                  f'# Laser on:        {self.led["laser on (LED1)"]}',
+                  '#',
+                  f'# Cell T error:    {self.sensor_par["cell temp error"]}',
+                  f'# Bz field:        {self.sensor_par["Bz field (pT)"]:.4f} pT',
+                  f'# By field:        {self.sensor_par["By field (pT)"]:.4f} pT',
+                  f'# B0 field:        {self.sensor_par["B0 field (pT)"]:.4f} pT',
+                   '#',
+                  ]
+
+        if notes:
+            notes = [f'#\t{note}' for note in notes]
+            header.extend(['# Notes', *notes, '#'])
+
+        header.extend([f'# {datetime.now()}', '# \n'])
+
+        with open(filename, 'w') as fid:
+            fid.write('\n'.join(header))
+
+        # write data
+        df.to_csv(filename, mode='a', index=False)
+
+
+
+def main():
+    fg = fluxgateLJ("fluxgate")
+    fg.setup(x=1,y=0,z=2)
+    dat = fg.read_single()
+    print(dat)
+
+
+if __name__=="__main__":
+    main()

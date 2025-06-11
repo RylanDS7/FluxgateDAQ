1e80840da175b47b88e4f8e4b5646e2ba7492d68
RylanDS7 <ryds105@gmail.com>
2025-06-10 20:35:39 -0700

    Automatic position recording added

--- src/fluxgateDAQ.py
+++ src/fluxgateDAQ.py
@@ -30,11 +30,13 @@ class fluxgateLJ:
         ch: (tuple): channel addresses for analog inputs
         handle (int): handle for sending info to labjack. Output of ljm.openS
         csv_log (bool): controls csv logging of measurements
-        filename: name of csv file to log to
+        filename (str): name of csv file to log to
+        increment (float): number of centimeters each measurement is seperated by; set to 0 if not used
+        position (float): current position of the measurement
     """
 
     def __init__(self, fluxgate_name=None, LJ_type='T7', LJ_connection='USB', LJ_id='ANY',
-                 fluxgate_nbytes=1000, csv_log=False):
+                 fluxgate_nbytes=1000, csv_log=False, increment=1):
         """Initialize object: connect
 
         Args:
@@ -45,12 +47,16 @@ class fluxgateLJ:
             LJ_connection (str): passed to ljm.openS, connection type. USB|ANY|ETHERNET
             LJ_identifier (str): passed to ljm.openS, device id
             fluxgate_nbytes (int): serial read chunk size in bytes for status updates
+            csv_log (bool): controls whether measurements are logged to csv
+            increment (float): number of centimeters each measurement is seperated by; set to 0 if not used
         """
 
         # get LJ handle
         self.handle = ljm.openS(deviceType=LJ_type,
                                 connectionType=LJ_connection,
                                 identifier=LJ_id)
+        
+        self.increment = increment
         
         # start logging csv
         if csv_log == True:
@@ -107,8 +113,19 @@ class fluxgateLJ:
         self.filename = f'fluxgate_{datetime.now().strftime("20%y-%m-%d_%H.%M.%S")}.csv'
 
         # write header of csv file
-        with open(self.filename, 'w', newline='') as csvfile:
-            csvfile.write("X,Y,Z\n")
+        if self.increment != 0:
+            with open(self.filename, 'w', newline='') as csvfile:
+                csvfile.write(datetime.now().strftime("20%y-%m-%d, %H:%M:%S\n\n"))
+                csvfile.write("Position (cm), B_x (uT), B_y (uT), B_z (uT)\n")
+
+        else:
+            with open(self.filename, 'w', newline='') as csvfile:
+                csvfile.write(datetime.now().strftime("20%y-%m-%d, %H:%M:%S\n\n"))
+                csvfile.write("B_x (uT), B_y (uT), B_z (uT)\n")
+                
+        
+        self.position = 0
+
     
 
     def log_csv(self, data):
@@ -117,6 +134,12 @@ class fluxgateLJ:
         Args:
             data (np array): data to be written to the csv file
         """
+
+        # add position to data line if increment enabled
+        if self.increment != 0:
+            data = np.insert(data, 0, self.position)
+            self.position += self.increment
+
         # write newline into csv file
         try:
             with open(self.filename, 'a', newline='') as csvfile:
